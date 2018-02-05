69d3957dec3cfae082f1ff76bc0547fa13b1199f
pedro-vicente <pedro.vicente@space-research.org>
2018-02-04 21:01:13 -0500

    add https_coin.cc

--- https_coin.cc
+++ https_coin.cc
@@ -1,10 +1,15 @@
 #include <string>
 #include <iostream>
 #include "ssl_socket.hh"
+#include "http.hh"
+#include "coin.hh"
+
+const char *host_name = "www.cryptocurrencychart.com";
 
 int main(int argc, char *argv[])
 {
   ssl_socket_t ssl;
+  currency_t curr;
  
   return 0;
 }
